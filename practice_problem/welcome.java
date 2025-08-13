1b99cd4336e22e851a6419f51848efa6ac40f593
PriyanshiG01 <priyanshi.goyal_cs24@gla.ac.in>
2025-08-13 14:19:21 +0530

    add practice folder

---
+++ practice_problem/welcome.java
@@ -0,0 +1,10 @@
+package priyanshi;
+import java.util.*;
+
+public class Welcome {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
