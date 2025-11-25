ecb5817b092da43776ce2a4852e221fb53da9572
PriyanshiG01 <priyanshi.goyal_cs24@gla.ac.in>
2025-11-25 13:32:40 +0530

    Update NotificationFilter.java

--- Lambda Expressions/NotificationFilter.java
+++ Lambda Expressions/NotificationFilter.java
@@ -1,5 +1,23 @@
 package priyanshi;
 
 public class NotificationFilter {
+String msg;
+    String type;
 
+    Alert(String m, String t) {
+        msg = m;
+        type = t;
+        public String toString () {
+            return msg + " (" + type + ")";
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Alert> alerts = Arrays.asList(
+                new Alert("Patient fever", "critical"),
+                new Alert("Medicine due", "info"),
+                new Alert("Emergency incident", "emergency")
+        );
+        Predicate<Alert> filter = alert -> alert.type.equals("critical") || alert.type.equals("emergency");
+        alerts.stream().filter(filter).forEach(System.out::println);
 }
