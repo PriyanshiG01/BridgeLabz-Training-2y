6b46c06254d3fb2647395dc3c772db508620aa52
PriyanshiG01 <priyanshi.goyal_cs24@gla.ac.in>
2025-11-25 13:34:53 +0530

    Update UppercaseNames.java

--- Lambda Expressions/UppercaseNames.java
+++ Lambda Expressions/UppercaseNames.java
@@ -1,5 +1,6 @@
 package priyanshi;
 
 public class UppercaseNames {
-
+ List<String> names = Arrays.asList("Alice", "Bob", "Carol");
+        names.stream().map(String::toUpperCase).forEach(System.out::println); 
 }
