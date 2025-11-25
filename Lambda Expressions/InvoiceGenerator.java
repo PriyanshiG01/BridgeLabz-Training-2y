c4d9736e639be213fb66fd6962c17f5ec5b0bed6
PriyanshiG01 <priyanshi.goyal_cs24@gla.ac.in>
2025-11-25 13:32:02 +0530

    Update InvoiceGenerator.java

--- Lambda Expressions/InvoiceGenerator.java
+++ Lambda Expressions/InvoiceGenerator.java
@@ -1,5 +1,15 @@
 package priyanshi;
 
 public class InvoiceGenerator {
+  interface LightAction { void activate(); }
 
+    public static void main(String[] args) {
+        Map<String, LightAction> actions = new HashMap<>();
+        actions.put("motion", () -> System.out.println("Turn light ON (motion detected)"));
+        actions.put("evening", () -> System.out.println("Set to warm dim mode (evening)"));
+        actions.put("voice", () -> System.out.println("Change color to blue (voice command)"));
+
+        actions.get("motion").activate();
+        actions.get("evening").activate();
+        actions.get("voice").activate();
 }
